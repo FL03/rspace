448227232b1185ff2aaca984757996b78c65926e
FL03 <jo3mccain@icloud.com>
2025-12-29 14:43:10 -0600

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- traits/src/container.rs
+++ traits/src/container.rs
@@ -1,9 +1,9 @@
 /*
-    Appellation: store <module>
-    Created At: 2025.12.26:14:12:46
+    Appellation: container <module>
+    Created At: 2025.12.29:14:39:20
     Contrib: @FL03
 */
-use crate::space::RawSpace;
+use crate::{Apply, RawSpace};
 
 /// The [`Container`] trait is a higher-kinded trait used to establish an interface for
 /// defining containers themselves.
@@ -12,6 +12,16 @@ where
     Self::Cont<U>: RawSpace<Elem = U>,
 {
     type Cont<V>: ?Sized;
+}
+
+pub trait ContainerMap<T>: Container<T>
+where
+    Self::Cont<T>: RawSpace<Elem = T>,
+{
+    fn map<F, X>(&self, f: F) -> Self::Cont<X>
+    where
+        Self::Cont<T>: Apply<F, Output = Self::Cont<X>>,
+        Self::Cont<X>: Sized;
 }
 
 pub trait ContainerIter<U>: Container<U>
@@ -27,6 +37,20 @@ where
 /*
  ************* Implementations *************
 */
+impl<S, T> ContainerMap<T> for S
+where
+    S: Container<T, Cont<T> = S>,
+    S::Cont<T>: RawSpace<Elem = T>,
+{
+    fn map<F, X>(&self, f: F) -> Self::Cont<X>
+    where
+        Self::Cont<T>: Apply<F, Output = Self::Cont<X>>,
+        Self::Cont<X>: Sized,
+    {
+        <Self::Cont<T> as Apply<F>>::apply(self, f)
+    }
+}
+
 impl<S, T> Container<T> for S
 where
     S: RawSpace<Elem = T>,
