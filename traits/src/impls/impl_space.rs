88d2296bd9fe7c6b8f40da83d1fe129e8e1efa6a
FL03 <jo3mccain@icloud.com>
2026-01-06 15:54:06 -0600

    update impl_space.rs
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- traits/src/impls/impl_space.rs
+++ traits/src/impls/impl_space.rs
@@ -51,6 +51,7 @@ impl_scalar_space! {
         bool, char
     }
 }
+
 #[cfg(feature = "alloc")]
 impl_scalar_space! {
     impl RawSpace for {
@@ -132,6 +133,13 @@ impl_raw_space! {
     impl<Elem = T> RawSpace for {
         hashbrown::HashMap<K, T, S>,
         hashbrown::HashSet<T, S>,
+    }
+}
+
+#[cfg(feature = "ndarray")]
+impl_raw_space! {
+    impl<Elem = T> RawSpace for {
+        ndarray::ArrayBase<S, D, T> { where S: ndarray::Data<Elem = T>, D: ndarray::Dimension },
     }
 }
 
