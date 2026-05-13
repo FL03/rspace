90c4bb69bf814f2bd0bd062802725a14ae1a98a4
FL03 <joe@pzzld.org>
2026-05-13 07:44:57 -0500

    v0.1.1
    Signed-off-by: FL03 <joe@pzzld.org>

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       in rec {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "rspace";
-          version = "0.1.0";
+          version = "0.1.1";
           src = self; # ./.;
           cargoLock = {
             lockFile = ./Cargo.lock;
